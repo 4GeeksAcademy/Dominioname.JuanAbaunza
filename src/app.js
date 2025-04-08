6efd0b2825b09c2637896cc476751e6fb9bb095a
JuanMa305 <jake_nel@hotmail.com>
2025-04-08 03:36:30 +0000

    cambié el let por const ya que los arrays no se modificaran y se mantendran constantes

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
   console.log("Hello Rigo from the console!");
 
-  for (let a of pronoun) {
-    for (let b of adj) {
-      for (let c of noun) {
+  for (const a of pronoun) {
+    for (const b of adj) {
+      for (const c of noun) {
         console.log(a + b + c + '.com');
       }
       
